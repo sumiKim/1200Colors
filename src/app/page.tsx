77c42468d348d08ba426ad087bc60308239ee5fb
Sumi <charlette7@naver.com>
2023-05-17 11:25:16 +0900

    '를 &apos;로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,12 @@ import BottomImage from './resource/image/2.png';
 import Banner1 from './resource/image/3.jpg';
 import Banner2 from './resource/image/4.jpeg';
 import Banner3 from './resource/image/5.jpeg';
-export default async function HomePage() {
+
+export default function HomePage() {
   return (
     <div className='flex flex-col'>
-      {/* <div className='sm:bg-yellow-400 md:bg-green-600  lg:bg-blue-600 xl:bg-violet-900 2xl:bg-red-600 text-white'>
+      {/* 
+      <div className='sm:bg-yellow-400 md:bg-green-600  lg:bg-blue-600 xl:bg-violet-900 2xl:bg-red-600 text-white'>
         반응형<br></br>
         2xl: 빨 <br></br>
         xl: 보<br></br>
@@ -19,7 +21,7 @@ export default async function HomePage() {
       <Image
         className='w-full h-screen object-cover'
         src={TopImage}
-        alt={'배경'}
+        alt='배경'
       />
       <div className='absolute left-20 bottom-28 text-white'>
         <div className='text-brand text-lg'>SAMHWA</div>
@@ -62,9 +64,9 @@ export default async function HomePage() {
         </div>
       </section>
       <section className='flex px-10 xl:px-32 2xl:px-64 py-36 gap-5'>
-        <MainBanner image={Banner1} title={'컬러'} link={'/color'} />
-        <MainBanner image={Banner2} title={'배색'} link={'/colorschema'} />
-        <MainBanner image={Banner3} title={'공간'} link={'/place'} />
+        <MainBanner image={Banner1} title='컬러' link='/color' />
+        <MainBanner image={Banner2} title='배색' link='/colorschema' />
+        <MainBanner image={Banner3} title='공간' link='/place' />
       </section>
       <section className='flex flex-col md:flex-row xl:flex-col'>
         <div className='flex flex-col basis-1/2 xl:flex-row px-10 xl:px-32 2xl:px-64 pt-8 pb-10 bg-[#666666] text-white'>
@@ -73,9 +75,9 @@ export default async function HomePage() {
               About
             </p>
             <p className='text-xs md:text-md font-thin'>
-              자사 컬러북인 'SAMHWA-NCS COLLECTION'
+              자사 컬러북인 &apos;SAMHWA-NCS COLLECTION&apos;
               <br></br>
-              'SAMHWA-NCS COLOR MASTER 950'과 600'
+              &apos;SAMHWA-NCS COLOR MASTER 950&apos;과 &apos;600&apos;
               <br></br>의 베스트 컬러, 시장수요가 많은 스테디 컬러를 <br></br>
               선별하고 각 분야의 트랜드가 반영된 신규컬러를 <br></br>
               추가하여 1200컬러로 구성하였습니다.
@@ -87,8 +89,9 @@ export default async function HomePage() {
             </p>
             <div className='text-xs md:text-md font-thin'>
               <div>
-                신규컬러는 <span className='font-semibold'>풍부</span>하고{' '}
-                <span className='font-semibold'>섬세</span>한 단계의 컬러구성을
+                신규컬러는 <span className='font-semibold'>풍부</span>
+                하고 <span className='font-semibold'>섬세</span>한 단계의
+                컬러구성을
               </div>
               <div>느낄 수 있으며 전문가와 일반소비자 모두가</div>
               <div>활용할 수 있는 컬러로 개발 되었습니다.</div>
@@ -110,7 +113,7 @@ export default async function HomePage() {
           <Image
             className='w-full h-full object-cover'
             src={BottomImage}
-            alt={'배경'}
+            alt='배경'
           />
         </div>
       </section>
