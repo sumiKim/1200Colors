200ae9b44afe7cd0078276e5079830d263d6f23c
Sumi <charlette7@naver.com>
2023-05-26 16:48:20 +0900

    검색 보여주는 방식 변경 - 연관색채

--- src/app/color/page.tsx
+++ src/app/color/page.tsx
@@ -1,10 +1,11 @@
 'use client';
 import useSWR from 'swr';
 import ChipDefault from '../components/ui/colorchips/ChipDefault';
-import { ResColors } from '@/service/type';
+import { Color, ResColors } from '@/service/type';
 import Error from '../components/ui/Error';
 import { makeReqUrlForColor, useSearchBox } from '../context/SearchBoxContext';
 import NoSearchResults from '../components/ui/NoSearchResults';
+import { HiColorSwatch } from 'react-icons/hi';
 
 export default function ColorPage() {
   const { searchType, searchKeyword } = useSearchBox();
@@ -12,10 +13,6 @@ export default function ColorPage() {
   const { data, isLoading, error } = useSWR<ResColors>(reqUrl !== '' && reqUrl);
 
   let colors = data?.result.resColor;
-
-  if (colors !== undefined && data?.result.nearbyColor !== undefined) {
-    colors = [...colors, ...data?.result.nearbyColor]; // 중복 칼라 있음.
-  }
 
   return (
     <div className='bg-white h-screen lg:pl-60 lg:pr-5 lg:pt-4 flex justify-center'>
@@ -26,13 +23,36 @@ export default function ColorPage() {
             <NoSearchResults type={searchType} keyword={searchKeyword} />
           )}
           {colors && (
-            <ul className='grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-12 text-center gap-2 p-2 w-fit '>
+            <ul className='grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-12 text-center gap-2 p-2 w-fit'>
               {colors.map(color => (
                 <li key={color.samwha_code} data-value={color.id}>
                   <ChipDefault color={color} />
                 </li>
               ))}
+              {/* {data?.result.nearbyColor !== undefined &&
+                data?.result.nearbyColor.map(color => (
+                  <li key={color.samwha_code} data-value={color.id}>
+                    <ChipDefault color={color} />
+                  </li>
+                ))} */}
             </ul>
+          )}
+          {colors?.length === 1 && data?.result.nearbyColor !== undefined && (
+            <>
+              <div className='relative w-full border-b-2 mt-3 mb-2'>
+                <p className='flex items-center pt-2 text-lg text-sh_text89 ps-2 gap-2'>
+                  <HiColorSwatch />
+                  <span>연관 색채</span>
+                </p>
+              </div>
+              <ul className='grid grid-cols-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-12 text-center gap-2 p-2 w-fit'>
+                {data?.result.nearbyColor.map(color => (
+                  <li key={color.samwha_code} data-value={color.id}>
+                    <ChipDefault color={color} />
+                  </li>
+                ))}
+              </ul>
+            </>
           )}
         </section>
       </div>
---
+++ src/app/components/ui/icons/ColorSwatch.tsx
@@ -0,0 +1,5 @@
+import { HiColorSwatch } from 'react-icons/hi';
+
+export default function ColorSwatch() {
+  return <HiColorSwatch className='w-5 h-5' />;
+}
